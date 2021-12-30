9378365953683b307cb4b843a889454c230facba
Morry <morandomatteo98@gmail.com>
2021-12-30 18:37:40 +0100

    update main to test sql parser

--- main.py
+++ main.py
@@ -1,22 +1,60 @@
+import sqlparse
+
 from lib.engine import Engine
-import os
 from loguru import logger
 
-db_path = "sqlite:///test.db"
+from lib.sql_parser.sql_parser_test import SqlParserTest
+from lib.sql_parser import sql_parser
+
+db_path = "sqlite:///D:\\PersonalProject\\ciuf\\test.db"
 logger.info(db_path)
 
-x = Engine(db_path)
-x.update_dag("""
-CREATE VIEW test AS
-SELECT story_id, COUNT(*) AS vcount
-FROM votes GROUP BY story_id;
-""")
-x.update_dag("""
-CREATE VIEW StoriesWithVC AS
-SELECT id, author, title, url, vcount
-FROM stories
-JOIN VoteCount ON VoteCount.story_id = stories.id
-WHERE stories.id = ?;
-""")
+# x = Engine(db_path)
+#
+# x.update_dag(
+#     """
+#     CREATE VIEW test AS
+#     SELECT story_id, COUNT(*) AS vcount
+#     FROM votes GROUP BY story_id;
+#     """
+# )
+#
+# x.update_dag(
+#     """
+#     CREATE VIEW StoriesWithVC AS
+#     SELECT id, author, title, url, vcount
+#     FROM stories
+#     JOIN VoteCount ON VoteCount.story_id = stories.id
+#     WHERE stories.id = ?;
+#     """
+# )
+#
+# x.print_dag()
 
-x.print_dag()
+
+
+# Old
+# sql_parser_test = SqlParserTest()
+#
+# sql_parser_test.query = """SELECT v.story_id, COUNT(v.pippo) AS aa
+#      FROM votes as v;
+#     """
+#
+# sql_parser_test.test(sqlparse.parse("""SELECT v.story_id, COUNT(v.pippo) AS aa
+#      FROM votes as v;
+#     """)[0])
+# sql_parser_test.print()
+#
+# print("\n\n\n")
+#
+# print(f"{sql_parser_test.tables=}")
+# print(f"{sql_parser_test.functions=}")
+# print(f"{sql_parser_test.columns=}")
+
+
+tables = sql_parser.parse_query("""SELECT v.story_id, COUNT(v.pippo) AS aa
+     FROM votes as v;
+     """)
+
+for table in tables:
+    print(table)
