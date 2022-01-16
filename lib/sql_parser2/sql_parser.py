80e840c8c7a323a5854de3962759b45a027509b6
Morry <morandomatteo98@gmail.com>
2022-01-16 17:16:29 +0100

    implement new sql_parser

---
+++ lib/sql_parser2/sql_parser.py
@@ -0,0 +1,26 @@
+from typing import List, Dict, Optional
+
+from lib.sql_parser.condition import Condition
+from lib.sql_parser.query import Query
+from lib.sql_parser.table import Table
+from lib.sql_parser2 import word_recognition
+from lib.sql_parser2.configurations import Configurations
+
+
+def parse_query(query_str: str) -> Query:
+    config = Configurations(query=Query(text=query_str))
+    query_str = query_str.replace('\n', '')
+    for word in query_str.split(' '):
+        if word == '':
+            continue
+        result, config = word_recognition.compute(word=word, config=config)
+        if not result:
+            raise Exception(f'{word} Statement not implemented')
+
+    query = config.query
+    for table in query.tables:
+        table.block_table()
+    for condition in query.condition:
+        condition.block_condition()
+    query.block_query()
+    return query
