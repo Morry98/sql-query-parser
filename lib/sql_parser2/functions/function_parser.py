82f45e402cf729ecdc068c02bbeb604e75526370
Morry <morandomatteo98@gmail.com>
2022-01-29 18:15:15 +0100

    refactor parsers

--- lib/sql_parser2/functions/function_parser.py
+++ lib/sql_parser2/functions/function_parser.py
@@ -8,7 +8,7 @@ def compute(word: str, config: Configurations) -> Tuple[bool, Configurations]:
     if type(word) is not str:
         raise Exception('word must be a string!')
     for class_ in functions.__dict__.items():
-        if class_[0].startswith("_"):
+        if class_[0].startswith("_") or class_[0] == "function_parser":
             continue
         result, config = class_[1].compute(word=word, config=config)
         if result is True:
