fd7d16b7e505ff3c3ee69338c92ffeb50e69a76f
Morry <morandomatteo98@gmail.com>
2022-03-05 23:08:07 +0100

    implement exceptions package
    -  ObjectBlockedException
    - DifferentConditionsInParenthesisException

---
+++ lib/exceptions/different_conditions_in_parenthesis_exception.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class DifferentConditionsInParenthesisException(Exception):
+    """Exception raised when in a parenthesis there are different conditions.
+    Ad example: WHERE a = 1 and b = 1 or c = 1
+
+    Attributes:
+        message: explanation of the error
+    """
+
+    def __init__(self, message: str = "There are different conditions type in a parenthesis"):
+        self.message: str = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        string = f"Message: {self.message}"
+        return string
