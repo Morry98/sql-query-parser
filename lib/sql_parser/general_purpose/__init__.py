61429147e0112ed601c9529989b18745cd1ce5ef
Morry <morandomatteo98@gmail.com>
2022-03-05 23:21:28 +0100

    implement packages __init__

---
+++ lib/sql_parser/general_purpose/__init__.py
@@ -0,0 +1,3 @@
+from lib.sql_parser.general_purpose import select_column
+from lib.sql_parser.general_purpose import from_table
+from lib.sql_parser.general_purpose import where_condition
---
+++ lib/sql_parser/keywords/__init__.py
@@ -0,0 +1,3 @@
+from lib.sql_parser.keywords import select
+from lib.sql_parser.keywords import from_
+from lib.sql_parser.keywords import where
