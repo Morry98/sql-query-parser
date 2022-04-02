b48656e159f18d8a477f420631174a1716ed3318
Morry <morandomatteo98@gmail.com>
2022-04-02 19:48:24 +0200

    implement select test

---
+++ lib/sql_parser/keywords/select_test.py
@@ -0,0 +1,29 @@
+from copy import copy
+from unittest import TestCase
+from lib.sql_parser.keywords import select
+
+from lib.sql_parser.configurations import Configurations
+from lib.sql_parser.query import Query
+
+
+class SelectTest(TestCase):
+
+    def test_no_select_word(self):
+        configuration = Configurations(query=Query(text=""))
+        result = select.compute(word="", config=copy(configuration))
+        self.assertEqual(2, len(result))
+        self.assertEqual(False, result[0])
+        self.assertEqual(configuration, result[1])
+
+    def test_select_word(self):
+        configuration = Configurations(query=Query(text=""))
+        result = select.compute(word="select", config=configuration)
+        self.assertEqual(2, len(result))
+        self.assertEqual(True, result[0])
+        self.assertEqual(configuration, result[1])
+
+    def test_select_is_not_first_keyword(self):
+        configuration = Configurations(query=Query(text=""))
+        configuration.add_keyword(keyword="from")
+        with self.assertRaises(Exception):
+            _ = select.compute(word="select", config=copy(configuration))
